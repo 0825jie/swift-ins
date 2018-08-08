062193a1f4922162d56ddf61123f2e51699a282f
0825jie <876999351@qq.com>
2018-08-08 09:24:15 -0700

    firebase

---
+++ swift-ins/Ins/Utility/Extension.swift
@@ -0,0 +1,18 @@
+//
+//  extension.swift
+//  Ins
+//
+//  Created by jason on 2018/8/7.
+//  Copyright © 2018年 jason. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    
+}
+
